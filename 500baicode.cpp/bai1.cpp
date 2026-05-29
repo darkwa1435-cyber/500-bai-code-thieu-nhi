988c3fff7b65935bc8ed324e9249c33f311c467e
TranNguyenTanDung <darkwa1435@gmail.com>
2026-05-29 18:00:28 +0700

    Bai 1

---
+++ 500baicode.cpp/bai1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void print(int a[], int lo, int hi){
+    for(int i = lo; i <= hi; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+void findpair(int a[], int sum, int lo, int hi){
+    for(int i = lo; i < hi; i++){
+        for(int j = i + 1; j <= hi; j++){
+            if (a[i] + a[j] == sum) cout << "{" << a[i] << "," << a[j] << "}";
+        }
+    }
+}
+int main(){
+    int a[8] = {2,6,8,9,7,5,1,4};
+    print(a,0,7);
+    findpair(a,14,0,7);
+}

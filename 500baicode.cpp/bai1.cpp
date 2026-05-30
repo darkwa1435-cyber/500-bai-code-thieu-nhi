5336f227b998cc214933c5e5589b75470a1dcc3d
TranNguyenTanDung <darkwa1435@gmail.com>
2026-05-30 21:01:24 +0700

    them truong hop khong co cap

--- 500baicode.cpp/bai1.cpp
+++ 500baicode.cpp/bai1.cpp
@@ -10,11 +10,14 @@ void findpair(int a[], int sum, int lo, int hi){
     for(int i = lo; i < hi; i++){
         for(int j = i + 1; j <= hi; j++){
             if (a[i] + a[j] == sum) cout << "{" << a[i] << "," << a[j] << "}";
+            else sum = -1;
         }
     }
+    if (sum = -1) cout << "Pair Not found";
 }
+
 int main(){
     int a[8] = {2,6,8,9,7,5,1,4};
     print(a,0,7);
-    findpair(a,14,0,7);
+    findpair(a,84,0,7);
 }
